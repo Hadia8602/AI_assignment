f8be6be2a5705b8362c9a64ddb498edd86df6480
Hadia8602 <hadiaishaq932@gmail.com>
2025-12-05 22:33:30 +0500

    Add new files

--- c-.cpp/question 25/main.cpp
+++ c-.cpp/question 25/main.cpp
@@ -1,6 +1,6 @@
 //25.	Write a program to input the values of three sides of a triangle and calculate its area using the formula:
 // Area = sqrt(s*((s-a)*(s-b)*(s-c)))
-// s = (a+b+c)/25
+// s = (a+b+c)/2
 #include<iostream>
 #include<cmath>
 using namespace std;
@@ -9,23 +9,23 @@ float sides(float a, float b, float c)
     float s = (a+b+c)/2;
     return s;
 }
-float area(float S)
+float area(float s, float a, float b, float c)
 {
-    float s, a, b, c;
+    
    float A = sqrt(s*((s-a)*(s-b)*(s-c)));
    return A;
 }
 int main()
 {
-    float b, c, S, a;
+    float a, b, c, s, Area;
     cout<<"enter the sides of trangle a:"<<endl;
     cin>>a;
     cout<<"enter the sides of trangle b:"<<endl;
     cin>>b;
     cout<<"enter the sides of trangle c:"<<endl;
     cin>>c;
-    S= sides(a,b,c);
-    a = area(S);
-    cout<<"the area of triangle is :\t"<< a << endl;
+    s= sides(a,b,c);
+    Area = area(s, a, b, c);
+    cout<<"the area of triangle is :\t"<< Area << endl;
     return 0;
 }
